8aa54a5a36bc6bc962e456aba05ef04fdbb1f705
shibichakkaravarthy <shibi2591999@gmail.com>
2021-05-27 17:27:08 +0530

    changed localhost to heroku

--- src/modules/auth/api.js
+++ src/modules/auth/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const signin = (emailId, password) => {
-    return axios.post("http://localhost:5000/auth/signin", {emailId, password})
+    return axios.post("https://nyx-learning-backend.herokuapp.com/auth/signin", {emailId, password})
 }
 
 export const signup = (emailId, password, role) => {
-    return axios.post("http://localhost:5000/auth/signup", {emailId, password, role})
+    return axios.post("https://nyx-learning-backend.herokuapp.com/auth/signup", {emailId, password, role})
 }
 
 export const refreshToken = (token) => {
-    return axios.post("http://localhost:5000/auth/refresh", {}, {headers: {"Authorization": token}});
+    return axios.post("https://nyx-learning-backend.herokuapp.com//auth/refresh", {}, {headers: {"Authorization": token}});
 }
--- src/modules/courses/api.js
+++ src/modules/courses/api.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
 export const getEnrolledCourses = (token) => {
-    return axios.get("http://localhost:5000/protected/student/enrolledCourses", {headers: {"Authorization": "Bearer "+token}})
+    return axios.get("https://nyx-learning-backend.herokuapp.com/protected/student/enrolledCourses", {headers: {"Authorization": "Bearer "+token}})
 }
 
 export const getAllCourses = (token) => {
-    return axios.get("http://localhost:5000/protected/student/allCourses", {headers: {"Authorization": "Bearer "+token}})
+    return axios.get("https://nyx-learning-backend.herokuapp.com/protected/student/allCourses", {headers: {"Authorization": "Bearer "+token}})
 }
 
 export const getCreatedCourses = (token) => {
-    return axios.get("http://localhost:5000/protected/faculty/courses", {headers: {"Authorization": "Bearer "+token}})
+    return axios.get("https://nyx-learning-backend.herokuapp.com/protected/faculty/courses", {headers: {"Authorization": "Bearer "+token}})
 }
 
 export const enroll = (courseId, token) => {
-    return axios.patch("http://localhost:5000/protected/student/enroll/"+courseId, {}, {headers: {"Authorization": "Bearer "+token}})
+    return axios.patch("https://nyx-learning-backend.herokuapp.com/protected/student/enroll/"+courseId, {}, {headers: {"Authorization": "Bearer "+token}})
 }
 
 export const createCourse = (courseDetails, token) => {
-    return axios.post("http://localhost:5000/protected/faculty/courses/create",{...courseDetails}, {headers: {"Authorization": "Bearer "+token}})
+    return axios.post("https://nyx-learning-backend.herokuapp.com/protected/faculty/courses/create",{...courseDetails}, {headers: {"Authorization": "Bearer "+token}})
 }
